ada9f762605ac26c42363b393caa0a8c79115454
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-22 15:50:23 +0100

    render homepage with route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('home');
+    $home = 'HomePage';
+    return view('home', compact('home'));
 })->name('home');
 
 
